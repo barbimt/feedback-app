ee39d1a1478ba0ac8710c9ef1f6d39c569853ace
barbimt <82407633+barbimt@users.noreply.github.com>
2022-05-04 16:10:35 -0300

    deleteFeedback Event & Prop Drilling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p>no Feedback yet</p>
     }
@@ -10,7 +10,7 @@ function FeedbackList({feedback}) {
     <div className='feedback-list'>
       {feedback.map((item) => (
 
-          <FeedbackItem key={item.id} item={item}/> 
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/> 
       ))}
          
       
