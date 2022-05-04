658e26a4b68754bdbd3471275b4221febebdfccd
barbimt <82407633+barbimt@users.noreply.github.com>
2022-05-04 15:06:27 -0300

    card Component and conditional styles

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 function Card({children, reverse}) {
 //   return (
@@ -11,4 +12,12 @@ return(
 )
 }
 
+Card.defaultProps= {
+    reverse: false, 
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool,
+}
 export default Card
