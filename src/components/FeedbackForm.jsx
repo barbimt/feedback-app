8d42aaed4a6cbbeccf604b0804e2d1248918a685
barbimt <82407633+barbimt@users.noreply.github.com>
2022-05-09 13:48:18 -0300

    add Feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from "./shared/Card"
 import Button from "./shared/Button"
 import RatingSelect from "./RatingSelect"
 
-export default function FeedbackForm() {
+export default function FeedbackForm({handleAdd}) {
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
 
@@ -25,9 +25,21 @@ export default function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit=(e) => {
+      e.preventDefault()
+      if(text.trim().length> 10 ){
+        const newFeedback = {
+          text,
+          rating
+        }
+        handleAdd(newFeedback)
+        setText('')
+      }
+    }
+
   return (
     <Card>
-      <form >
+      <form onSubmit={handleSubmit}>
           <h2>How would you rate your service with us?</h2>
           <RatingSelect select={(rating)=> setRating(rating)}/>
           <div className="input-group">
